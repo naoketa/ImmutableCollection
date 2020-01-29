e8639d41dd4ef1e0b6b9efb5e5e4b903d77557a4
naoketa <49198895+naoketa@users.noreply.github.com>
2020-01-29 23:08:28 +0900

    reverseの末尾最適化
    タグずれ & addAll

--- src/main/scala/List.scala
+++ src/main/scala/List.scala
@@ -3,8 +3,6 @@ sealed trait List[+T] {
 
   def add[E >: T](item: E): List[E]
 
-  def addAll[E >: T](items: List[E]): List[E]
-
   def head: T
 
   def tail: List[T]
@@ -14,11 +12,6 @@ case object EmptyList extends List[Nothing] {
   override def isEmpty: Boolean = true
 
   override def add[E >: Nothing](item: E): List[E] = NonEmptyList(item, EmptyList)
-
-  override def addAll[E >: Nothing](items: List[E]): List[E] = items match {
-    case EmptyList => this
-    case NonEmptyList(x: E, xs: List[E]) => NonEmptyList(items.head, items.tail)
-  }
 
   override def head: Nothing = throw new NoSuchElementException("EmptyList dose not have any items.")
 
@@ -30,16 +23,16 @@ final case class NonEmptyList[T](head: T, tail: List[T]) extends List[T] {
 
   override def add[E >: T](item: E): List[E] = NonEmptyList(item, this)
 
-  override def addAll[E >: T](items: List[E]): List[E] = items match {
-    case EmptyList => this
-    case NonEmptyList(x: T, xs: List[T]) => this.addAll(xs).add(x)
-  }
 }
 
 object List {
-  def reverse[T](xs: List[T]): List[T] = xs match {
-    case EmptyList => EmptyList
-    case NonEmptyList(x: T, xs: List[T]) => NonEmptyList(x, EmptyList).addAll(reverse(xs))
+
+  def reverse[T](items: List[T]): List[T] = reverse(items, EmptyList)
+
+  @scala.annotation.tailrec
+  private def reverse[T](items: List[T], accumulator: List[T]): List[T] = items match {
+    case EmptyList => accumulator
+    case NonEmptyList(x: T, xs: List[T]) => reverse(items.tail, accumulator.add(items.head))
   }
 
   def apply[T](items: T*): List[T] =
--- src/test/scala/ListSpec.scala
+++ src/test/scala/ListSpec.scala
@@ -27,60 +27,37 @@ class ListSpec extends AnyFunSpec {
         }
       }
 
-      it("should have the list when addAll a list") {
-        val list: List[Int] = List(1, 2, 3)
-        val xs: List[Int] = List().addAll(list)
-        assert(xs.head === 1)
-        assert(xs.tail.head === 2)
-        assert(xs.tail.tail.head === 3)
-      }
-
-      it("should isEmpty when addAll a empty list") {
-        val list: List[Int] = List()
-        val xs: List[Int] = List().addAll(list)
-        assert(xs.isEmpty)
-      }
-
       it("should EmptyList when reversed") {
         val xs: List[Int] = List()
         assert(List.reverse(xs) === EmptyList)
       }
     }
-  }
 
-  describe("NonEmptyList") {
-    it("should not isEmpty") {
-      val xs = List(1)
-      assert(!xs.isEmpty)
-    }
 
-    it("should return first value when head") {
-      val xs = List(1)
-      assert(xs.head === 1)
-    }
+    describe("NonEmptyList") {
+      it("should not isEmpty") {
+        val xs = List(1)
+        assert(!xs.isEmpty)
+      }
 
-    it("should return value without head when tail") {
-      val xs = List(1).add(2)
-      assert(xs.tail.head === 1)
-      assert(xs.tail.tail == EmptyList)
-    }
+      it("should return first value when head") {
+        val xs = List(1)
+        assert(xs.head === 1)
+      }
 
-    it("should have the list when addAll a list") {
-      val list: List[Int] = List(1, 2)
-      val xs: List[Int] = List(3, 4).addAll(list)
-      assert(xs.head === 1)
-      assert(xs.tail.head === 2)
-      assert(xs.tail.tail.head === 3)
-      assert(xs.tail.tail.tail.head === 4)
-    }
+      it("should return value without head when tail") {
+        val xs = List(1).add(2)
+        assert(xs.tail.head === 1)
+        assert(xs.tail.tail == EmptyList)
+      }
 
-    it("should return the reversed list the list when reverse") {
-      val xs: List[Int] = List.reverse(List(1, 2, 3, 4))
-      assert(xs.head === 4)
-      assert(xs.tail.head === 3)
-      assert(xs.tail.tail.head === 2)
-      assert(xs.tail.tail.tail.head === 1)
+      it("should return the reversed list the list when reverse") {
+        val xs: List[Int] = List.reverse(List(1, 2, 3, 4))
+        assert(xs.head === 4)
+        assert(xs.tail.head === 3)
+        assert(xs.tail.tail.head === 2)
+        assert(xs.tail.tail.tail.head === 1)
+      }
     }
   }
-
 }
