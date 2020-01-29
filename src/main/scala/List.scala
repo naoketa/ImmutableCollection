39145b030a2bd44f28d721a3860a120d1749317c
naoketa <49198895+naoketa@users.noreply.github.com>
2020-01-29 22:20:38 +0900

    head,tail,addの実装

--- src/main/scala/List.scala
+++ src/main/scala/List.scala
@@ -1,13 +1,27 @@
 sealed trait List[+T] {
   def isEmpty: Boolean
+
+  def add[E >: T](item: E): List[E]
+
+  def head: T
+
+  def tail: List[T]
 }
 
 case object EmptyList extends List[Nothing] {
   override def isEmpty: Boolean = true
+
+  override def add[E >: Nothing](item: E): List[E] = NonEmptyList(item, EmptyList)
+
+  override def head: Nothing = throw new NoSuchElementException("EmptyList dose not have any items.")
+
+  override def tail: List[Nothing] = throw new NoSuchElementException("EmptyList dose not have any items.")
 }
 
 final case class NonEmptyList[T](head: T, tail: List[T]) extends List[T] {
   override def isEmpty: Boolean = false
+
+  override def add[E >: T](item: E): List[E] = NonEmptyList(item, this)
 }
 
 object List {
--- src/test/scala/ListSpec.scala
+++ src/test/scala/ListSpec.scala
@@ -7,13 +7,44 @@ class ListSpec extends AnyFunSpec {
         val xs = List()
         assert(xs.isEmpty)
       }
-    }
 
-    describe("NonEmptyList") {
-      it("should not isEmpty") {
-        val xs = List(1)
+      it("with a element should not isEmpty") {
+        val xs: List[Int] = List().add(1)
         assert(!xs.isEmpty)
+      }
+
+      it("should throw NoSuchElementException when head") {
+        val xs: List[Int] = List()
+        intercept[NoSuchElementException] {
+          xs.head
+        }
+      }
+
+      it("should throw NoSuchElementException when tail") {
+        val xs: List[Int] = List()
+        intercept[NoSuchElementException] {
+          xs.tail
+        }
       }
     }
   }
+
+  describe("NonEmptyList") {
+    it("should not isEmpty") {
+      val xs = List(1)
+      assert(!xs.isEmpty)
+    }
+
+    it("should return first value when head") {
+      val xs = List(1)
+      assert(xs.head === 1)
+    }
+
+    it("should return value without head when tail") {
+      val xs = List(1).add(2)
+      assert(xs.tail.head === 1)
+      assert(xs.tail.tail == EmptyList)
+    }
+  }
+
 }
